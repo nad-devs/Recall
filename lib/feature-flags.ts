b430cfe5b031f33644d2f8cd3ccaea620352f8bd
nad-devs <arjun.nadar.connect@gmail.com>
2025-06-13 14:01:11 +0400

    Add Knowledge Graph feature with feature flag

---
+++ lib/feature-flags.ts
@@ -0,0 +1,9 @@
+// Feature flags to control experimental features
+// These can be toggled through environment variables or directly here
+
+const featureFlags = {
+  // Knowledge Graph feature
+  enableKnowledgeGraph: true,
+};
+
+export default featureFlags; 
