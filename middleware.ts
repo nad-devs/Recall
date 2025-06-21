d1cf57bba61ffafcda33b9799df42044870ff7aa
nad-devs <arjun.nadar.connect@gmail.com>
2025-06-22 03:07:15 +0400

    Revert "fix(auth): exclude next-auth routes from middleware"
    This reverts commit 40280da36c0e3a0c163c5aae1c41adc47a49f093.

--- middleware.ts
+++ middleware.ts
@@ -60,11 +60,10 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api/auth (NextAuth routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api/auth|_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|favicon.ico).*)',
   ],
 } 
